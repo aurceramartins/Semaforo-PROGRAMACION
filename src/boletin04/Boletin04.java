53076882b7d948332db13aa3000fc6aa912c70bb
aurceramartins <aurceramartins@localhost>
2014-10-09 11:24:07 +0200

    Semaforo

--- src/boletin04/Boletin04.java
+++ src/boletin04/Boletin04.java
@@ -1,19 +1,11 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package boletin04;
-
-/**
- *
- * @author aurceramartins
- */
 public class Boletin04 {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Semaforo sema1=new Semaforo();
+        String valor=sema1.dameCor();
+        System.out.println("cor antes de darlle un valor: "+valor);
+        sema1.ponCor("verde");
+        valor=sema1.dameCor();
+        System.out.println("Cor: "+ valor);
     }
 }
