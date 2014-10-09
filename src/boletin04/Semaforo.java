790b0410e38a93ef8963159a06aae1a914640854
aurceramartins <aurceramartins@localhost>
2014-10-09 11:33:49 +0200

    Semaforo

---
+++ src/boletin04/Semaforo.java
@@ -0,0 +1,15 @@
+
+package boletin04;
+
+public class Semaforo {
+    private String cor;
+    public void ponCor(String c){
+        cor=c;
+    }
+    public String dameCor(){
+        return cor;
+    }
+    
+    
+}
+
