5e71cbb57bf0c326e03442aba43df951f76d7357
aurceramartins <aurceramartins@localhost>
2014-10-09 11:06:00 +0200

    commit

---
+++ src/boletin04/Boletin04.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin04;
+
+/**
+ *
+ * @author aurceramartins
+ */
+public class Boletin04 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
